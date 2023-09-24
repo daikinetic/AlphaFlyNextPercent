ff91b557cbec645e0cfe85d820e0e998d3703885
daikinetic <dddaiki.geek@gmail.com>
2023-09-25 00:11:59 +0900

    upd toggle contentTransition

--- AlphaFlyNextPercent/2023/ToggleContentTransition.swift
+++ AlphaFlyNextPercent/2023/ToggleContentTransition.swift
@@ -15,24 +15,63 @@ struct ToggleContentTransition: View {
   @State var videoOn: Bool = false
 
   var body: some View {
+    VStack {
+      Circle()
+        .frame(width: 180)
+        .overlay {
+          Image(systemName: "face.smiling")
+            .resizable()
+            .foregroundColor(.cyan)
+        }
+        .foregroundColor(.white)
+        .padding(.bottom, 70)
 
-    HStack(spacing: 80) {
-      Toggle(isOn: $audioOn) {
-        Image(systemName: audioOn ? "mic.fill" : "mic.slash.fill")
-          .symbolRenderingMode(.hierarchical)
-          .foregroundColor(audioOn ? .green : .primary)
-          .contentTransition(.symbolEffect(.replace))
-      }
-      .frame(width: 80)
 
-      Toggle(isOn: $videoOn) {
-        Image(systemName: videoOn ? "video.fill" : "video.slash.fill")
-          .symbolRenderingMode(.hierarchical)
-          .foregroundColor(videoOn ? .green : .primary)
-          .contentTransition(.symbolEffect(.replace))
+      Text("Set how you would like to join the meeting")
+        .foregroundColor(.white)
+        .padding(.bottom, 80)
+
+      HStack(spacing: 80) {
+        Toggle(isOn: $audioOn) {
+          Image(systemName: audioOn ? "mic.fill" : "mic.slash.fill")
+            .symbolRenderingMode(.hierarchical)
+            .foregroundColor(audioOn ? .green : .gray)
+            .contentTransition(.symbolEffect(.replace))
+        }
+        .frame(width: 70)
+
+        Toggle(isOn: $videoOn) {
+          Image(systemName: videoOn ? "video.fill" : "video.slash.fill")
+            .symbolRenderingMode(.hierarchical)
+            .foregroundColor(videoOn ? .green : .gray)
+            .contentTransition(.symbolEffect(.replace))
+        }
+        .frame(width: 80)
       }
-      .frame(width: 80)
+      .padding(.bottom, 80)
+
+
+      Button(
+        action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/,
+        label: {
+          Text("Join muted")
+            .frame(height: 50)
+            .frame(maxWidth: .infinity)
+            .foregroundColor(.white)
+            .background(Color.blue)
+            .cornerRadius(24)
+            .padding(.horizontal, 80)
+        }
+      )
+      .frame(height: 65)
+      .frame(maxWidth: .infinity)
+      .foregroundColor(.blue)
     }
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background {
+      Color.black
+    }
+    .ignoresSafeArea(.all)
   }
 }
 
