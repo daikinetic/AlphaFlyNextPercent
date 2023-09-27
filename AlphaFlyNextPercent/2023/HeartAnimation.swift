de4e8c23a6f5305b62a46add519fbf5ad8d7a5e7
daikinetic <dddaiki.geek@gmail.com>
2023-09-27 23:51:12 +0900

    HeartAnimation init

---
+++ AlphaFlyNextPercent/2023/HeartAnimation.swift
@@ -0,0 +1,18 @@
+//
+//  HeartAnimation.swift
+//  AlphaFlyNextPercent
+//
+//  Created by Daiki Takano on 2023/09/27.
+//
+
+import SwiftUI
+
+struct HeartAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeartAnimation()
+}
