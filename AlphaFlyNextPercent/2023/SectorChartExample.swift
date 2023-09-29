3c66b24dd96267b8058a2421d84b4c783c0b7cab
daikinetic <dddaiki.geek@gmail.com>
2023-09-29 23:46:49 +0900

    SectorChart

---
+++ AlphaFlyNextPercent/2023/SectorChartExample.swift
@@ -0,0 +1,45 @@
+//
+//  SectorChartExample.swift
+//  AlphaFlyNextPercent
+//
+//  Created by Daiki Takano on 2023/09/29.
+//
+//  https://x.com/mecid/status/1707347930623914260?s=20
+
+import SwiftUI
+import Charts
+
+struct SectorChartExample: View {
+  @State private var products: [Product] = [
+    .init(title: "Annual", revenue: 0.7),
+    .init(title: "Monthly", revenue: 0.2),
+    .init(title: "Lifetime", revenue: 0.1)
+  ]
+
+  var body: some View {
+    Chart(products) { product in
+      SectorMark(
+        angle: .value(
+          Text(verbatim: product.title),
+          product.revenue
+        )
+      )
+      .foregroundStyle(
+        by: .value(
+          Text(verbatim: product.title),
+          product.title
+        )
+      )
+    }
+  }
+}
+
+#Preview {
+  SectorChartExample()
+}
+
+struct Product: Identifiable {
+  let id = UUID()
+  let title: String
+  let revenue: Double
+}
