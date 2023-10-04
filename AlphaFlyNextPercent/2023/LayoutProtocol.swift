613e7321ee61fb5d8cc8aa0bf3c0a05a710025fa
daikinetic <dddaiki.geek@gmail.com>
2023-10-04 23:41:40 +0900

    WIP Layout Protocol

---
+++ AlphaFlyNextPercent/2023/LayoutProtocol.swift
@@ -0,0 +1,21 @@
+//
+//  LayoutProtocol.swift
+//  AlphaFlyNextPercent
+//
+//  Created by Daiki Takano on 2023/10/04.
+//
+//  https://x.com/mecid/status/1709164650695192996?s=20
+
+// the Layout protocol allowing us to build super-custom layouts
+// by digging into the layout system without using GeometryReader.
+// Layout protocol brings us the incredible power
+// of building and reusing any layout you can imagine.
+
+import Foundation
+import SwiftUI
+
+struct FlowLayout: Layout {
+  func sizeThatFits() {
+    
+  }
+}
