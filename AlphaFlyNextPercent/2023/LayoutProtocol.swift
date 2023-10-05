1c2bb3510fa3706f8763be172a6ac529a9335426
daikinetic <dddaiki.geek@gmail.com>
2023-10-05 11:52:33 +0900

    Layout Protocol

--- AlphaFlyNextPercent/2023/LayoutProtocol.swift
+++ AlphaFlyNextPercent/2023/LayoutProtocol.swift
@@ -15,7 +15,34 @@ import Foundation
 import SwiftUI
 
 struct FlowLayout: Layout {
+  func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+    //
+  }
+  
   func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
     let sizes = subviews.map { $0.sizeThatFits(.unspecified) }
+
+    var totalHeight: CGFloat = 0
+    var totalWidth: CGFloat = 0
+
+    var lineHeight: CGFloat = 0
+    var lineWidth: CGFloat = 0
+
+    for size in sizes {
+      if lineWidth + size.width > proposal.width ?? 0 {
+        totalHeight += lineHeight
+        lineWidth = size.width
+        lineHeight = size.height
+      } else {
+        lineWidth += size.width
+        lineHeight = max(lineHeight, size.height)
+      }
+
+      totalWidth = max(totalWidth, lineWidth)
+    }
+
+    totalHeight += lineHeight
+
+    return .init(width: totalWidth, height: totalHeight)
   }
 }
