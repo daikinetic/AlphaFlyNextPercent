261786759538676178dd7b9ab9ecdf9e845cff15
daikinetic <dddaiki.geek@gmail.com>
2023-10-11 00:22:22 +0900

    UIKit Preview

--- AlphaFlyNextPercent/2023/UIKitPreview.swift
+++ AlphaFlyNextPercent/2023/UIKitPreview.swift
@@ -7,20 +7,68 @@
 //  https://x.com/_take_hito_/status/1710290960360391026?s=20
 
 import UIKit
+import MondrianLayout
 
 class ViewController: UIViewController {
   let label: UILabel
   let imageView: UIImageView
+
+  init(label: UILabel, imageView: UIImageView) {
+    self.label = label
+    self.imageView = imageView
+
+    super.init()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  override func viewDidLoad() {
+    Mondrian.buildSubviews(on: view) {
+      VStackBlock {
+        imageView
+          .viewBlock.spacingAfter(10)
+
+        label
+      }
+    }
+  }
 }
 
 #Preview {
-  //
+
+  let font: UIFont = .scaledFont(style: .largeTitle, weight: .bold)
+  let label: UILabel = .init()
+  label.text = "さよなら歌姫"
+  label.font = font
+
+  let controller: ViewController = .init(
+    label: label,
+    imageView: .init()
+  )
+
+  controller.imageView.image = .init(systemName: "globe", font: font)
+
+  return controller
 }
 
 extension UIFont {
-  //
+  static func scaledFont(style: TextStyle, weight: Weight) -> UIFont {
+    UIFontMetrics(forTextStyle: style).scaledFont(
+      for: .systemFont(
+        ofSize: UIFont.preferredFont(forTextStyle: style).pointSize,
+        weight: weight
+      )
+    )
+  }
 }
 
 extension UIImage {
-
+  convenience init?(systemName: String, font: UIFont) {
+    self.init(
+      systemName: systemName,
+      withConfiguration: UIImage.SymbolConfiguration(font: font)
+    )
+  }
 }
