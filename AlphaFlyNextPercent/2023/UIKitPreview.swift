7edfa9c58f4b6ebce04175e5872fba0b97fe696f
daikinetic <dddaiki.geek@gmail.com>
2023-10-10 23:56:02 +0900

    UIKit Preview

---
+++ AlphaFlyNextPercent/2023/UIKitPreview.swift
@@ -0,0 +1,26 @@
+//
+//  UIKitPreview.swift
+//  AlphaFlyNextPercent
+//
+//  Created by Daiki Takano on 2023/10/10.
+//
+//  https://x.com/_take_hito_/status/1710290960360391026?s=20
+
+import UIKit
+
+class ViewController: UIViewController {
+  let label: UILabel
+  let imageView: UIImageView
+}
+
+#Preview {
+  //
+}
+
+extension UIFont {
+  //
+}
+
+extension UIImage {
+
+}
