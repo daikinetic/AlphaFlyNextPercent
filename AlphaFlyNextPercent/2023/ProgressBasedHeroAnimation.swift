f04451611cbf557491e46e6d6220b731c439594c
daikinetic <dddaiki.geek@gmail.com>
2023-10-15 00:07:19 +0900

    WIP ProgressBasedHeroAnimation

---
+++ AlphaFlyNextPercent/2023/ProgressBasedHeroAnimation.swift
@@ -0,0 +1,19 @@
+//
+//  ProgressBasedHeroAnimation.swift
+//  AlphaFlyNextPercent
+//
+//  Created by Daiki Takano on 2023/10/15.
+//
+//  https://youtu.be/1h5NjJbheEU?si=EJSjO__6-Xp1bOm6
+
+import SwiftUI
+
+struct ProgressBasedHeroAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressBasedHeroAnimation()
+}
