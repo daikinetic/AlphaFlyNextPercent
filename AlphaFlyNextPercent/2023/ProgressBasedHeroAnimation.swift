1f8ec17f60723e0d1297ccb877386606e1c6df0e
daikinetic <dddaiki.geek@gmail.com>
2023-10-15 23:27:00 +0900

    ProgressBasedHeroAnimation

--- AlphaFlyNextPercent/2023/ProgressBasedHeroAnimation.swift
+++ AlphaFlyNextPercent/2023/ProgressBasedHeroAnimation.swift
@@ -10,10 +10,113 @@ import SwiftUI
 
 struct ProgressBasedHeroAnimation: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      HomeProgressBasedHero()
     }
 }
 
 #Preview {
     ProgressBasedHeroAnimation()
 }
+
+struct HomeProgressBasedHero: View {
+
+  @State private var allProfiles: [Profile] = profiles
+  @State private var selectedProfile: Profile?
+  @State private var showDetail: Bool = false
+
+  var body: some View {
+    NavigationStack {
+      List(allProfiles) { profile in
+        HStack(spacing: 12) {
+          Image(profile.profilePicture)
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 50, height: 50)
+            .clipShape(.circle)
+
+          VStack(alignment: .leading, spacing: 6) {
+            Text(profile.userName)
+              .fontWeight(.semibold)
+
+            Text(profile.lastMsg)
+              .font(.caption2)
+              .foregroundStyle(.gray)
+          }
+        }
+        .contentShape(.rect)
+        .onTapGesture {
+          selectedProfile = profile
+          showDetail = true
+        }
+      }
+      .navigationTitle("Progress Effect")
+    }
+    .overlay {
+      DetailView(
+        selectedProfile: $selectedProfile,
+        showDetail: $showDetail
+      )
+    }
+  }
+}
+
+struct DetailView: View {
+
+  @Binding var selectedProfile: Profile?
+  @Binding var showDetail: Bool
+  @Environment(\.colorScheme) private var scheme
+
+  var body: some View {
+    if let selectedProfile, showDetail {
+      GeometryReader {
+        let size = $0.size
+
+        ScrollView {
+          Image(selectedProfile.profilePicture)
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: size.width, height: 400)
+            .clipped()
+        }
+        .scrollIndicators(.hidden)
+        .ignoresSafeArea()
+        .frame(width: size.width, height: size.height)
+        .background {
+          Rectangle()
+            .fill(scheme == .dark ? .black : .white)
+            .ignoresSafeArea()
+        }
+        // Close
+        .overlay(alignment: .topLeading) {
+          Button() {
+            showDetail = false
+            self.selectedProfile = nil
+          } label: {
+            Image(systemName: "xmark.circle.fill")
+              .font(.largeTitle)
+              .imageScale(.medium)
+              .contentShape(.rect)
+              .foregroundStyle(.white, .black)
+          }
+          .buttonStyle(.plain)
+          .padding()
+        }
+      }
+    }
+  }
+}
+
+struct Profile: Identifiable {
+  var id = UUID()
+  var userName: String
+  var profilePicture: String
+  var lastMsg: String
+}
+
+var profiles = [
+  Profile(userName: "Justine", profilePicture: "pic", lastMsg: "Hello, World"),
+  Profile(userName: "Justine", profilePicture: "mountain", lastMsg: "Hello, World"),
+  Profile(userName: "Justine", profilePicture: "pic", lastMsg: "Hello, World"),
+  Profile(userName: "Justine", profilePicture: "mountain", lastMsg: "Hello, World"),
+  Profile(userName: "Justine", profilePicture: "pic", lastMsg: "Hello, World")
+]
