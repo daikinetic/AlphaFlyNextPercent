881dd2aa6091a999b0290e2cce5c4c627677d4c1
daikinetic <dddaiki.geek@gmail.com>
2023-10-16 23:06:29 +0900

    add Anchor key

--- AlphaFlyNextPercent/2023/ProgressBasedHeroAnimation.swift
+++ AlphaFlyNextPercent/2023/ProgressBasedHeroAnimation.swift
@@ -120,3 +120,10 @@ var profiles = [
   Profile(userName: "Justine", profilePicture: "mountain", lastMsg: "Hello, World"),
   Profile(userName: "Justine", profilePicture: "pic", lastMsg: "Hello, World")
 ]
+
+struct AnchorKey: PreferenceKey {
+  static var defaultValue: [String: Anchor<CGRect>] = [:]
+  static func reduce(value: inout [String : Anchor<CGRect>], nextValue: () -> [String : Anchor<CGRect>]) {
+    value.merge(nextValue()) { $1 }
+  }
+}
