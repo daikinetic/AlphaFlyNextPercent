e7a378c6089581e9cc25306dbeea90610847ccd5
daikinetic <dddaiki.geek@gmail.com>
2023-10-22 19:57:31 +0900

    sensoryFeedback

---
+++ AlphaFlyNextPercent/2023/sensoryFeedBack.swift
@@ -0,0 +1,28 @@
+//
+//  sensoryFeedBack.swift
+//  AlphaFlyNextPercent
+//
+//  Created by Daiki Takano on 2023/10/22.
+//
+//  https://x.com/mecid/status/1712383218387767667?s=20
+
+import SwiftUI
+
+//struct sensoryFeedBack: View {
+//
+//  @State private var store = Store()
+//
+//  var body: some View {
+//    NavigationStack {
+//      List(store.results, id: \.self) { result in
+//        Text(result)
+//      }
+//      .seachable(text: $store.query)
+//      .sensoryFeedback(.success, trigger: store.results)
+//    }
+//  }
+//}
+//
+//#Preview {
+//  sensoryFeedBack()
+//}
