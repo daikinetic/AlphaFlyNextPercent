f7eb55a1cc2e79c6abfde9bcb1c8930fc5e1400e
daikinetic <dddaiki.geek@gmail.com>
2023-10-23 21:01:54 +0900

    enum feature

---
+++ AlphaFlyNextPercent/2023/EnumFeature.swift
@@ -0,0 +1,21 @@
+//
+//  EnumFeature.swift
+//  AlphaFlyNextPercent
+//
+//  Created by Daiki Takano on 2023/10/23.
+//
+//  https://youtu.be/SmE12g9aGpc?si=vvmBMi9KbXtQ1CYd
+
+import Foundation
+
+struct Constant {
+  static let baseUrl = URL(string: "https://myapi.com")
+  static let apiPath = "/api/v2/"
+  static let apiKey = "fdsadfcadcaca"
+}
+
+enum _Constant {
+  static let baseUrl = URL(string: "https://myapi.com")
+  static let apiPath = "/api/v2/"
+  static let apiKey = "fdsadfcadcaca"
+}
