8b779d0589af78c0c68bc2d28e641e67adb5610c
daikinetic <dddaiki.geek@gmail.com>
2023-10-24 22:08:29 +0900

    UIOnboarding

---
+++ AlphaFlyNextPercent/2023/UIOnboarding.swift
@@ -0,0 +1,9 @@
+//
+//  UIOnboarding.swift
+//  AlphaFlyNextPercent
+//
+//  Created by Daiki Takano on 2023/10/24.
+//
+//  https://github.com/lascic/UIOnboarding
+
+import Foundation
--- AlphaFlyNextPercent/MainTabBarViewController.swift
+++ AlphaFlyNextPercent/MainTabBarViewController.swift
@@ -7,8 +7,14 @@
 
 import UIKit
 import MondrianLayout
+import UIOnboarding
 
-class MainTabBarViewController: UITabBarController {
+class MainTabBarViewController: UITabBarController, UIOnboardingViewControllerDelegate {
+
+  @objc func didFinishOnboarding(onboardingViewController: UIOnboardingViewController) {
+    onboardingViewController.modalTransitionStyle = .crossDissolve
+    onboardingViewController.dismiss(animated: true, completion: nil)
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -20,6 +26,16 @@ class MainTabBarViewController: UITabBarController {
 
   func setUp() {
 
+    let onboardingController: UIOnboardingViewController = .init(withConfiguration: .init(
+      appIcon: UIOnboardingHelper.setUpIcon(),
+      firstTitleLine: UIOnboardingHelper.setUpFirstTitleLine(),
+      secondTitleLine: UIOnboardingHelper.setUpSecondTitleLine(),
+      features: UIOnboardingHelper.setUpFeatures(),
+      buttonConfiguration: UIOnboardingHelper.setUpButton()
+    ))
+    onboardingController.delegate = self
+    navigationController?.present(onboardingController, animated: false)
+
     let firstVC = MondrianOfMondrian()
     firstVC.tabBarItem = UITabBarItem(title: "tab1", image: .actions, tag: 0)
 
@@ -27,7 +43,7 @@ class MainTabBarViewController: UITabBarController {
     secondVC.tabBarItem = UITabBarItem(title: "tab2", image: .add, tag: 1)
 //    secondVC.view.backgroundColor = .layeringColor
 
-    let thirdVC = MockViewController()
+    let thirdVC = onboardingController
     thirdVC.tabBarItem = UITabBarItem(title: "tab3", image: .strokedCheckmark, tag: 2)
 
     viewControllers = [firstVC, secondVC, thirdVC]
@@ -72,3 +88,57 @@ class MainTabBarViewController: UITabBarController {
 
 }
 
+import UIKit
+import UIOnboarding
+
+struct UIOnboardingHelper {
+    // App Icon
+    static func setUpIcon() -> UIImage {
+      return Bundle.main.appIcon ?? .init(named: "mountain")!//.init(named: "onboarding-icon")!
+    }
+
+    // First Title Line
+    // Welcome Text
+    static func setUpFirstTitleLine() -> NSMutableAttributedString {
+        .init(string: "Welcome to", attributes: [.foregroundColor: UIColor.label])
+    }
+
+    // Second Title Line
+    // App Name
+    static func setUpSecondTitleLine() -> NSMutableAttributedString {
+        .init(string: Bundle.main.displayName ?? "Insignia", attributes: [
+          .foregroundColor: UIColor.blue
+        ])
+    }
+
+    // Core Features
+    static func setUpFeatures() -> Array<UIOnboardingFeature> {
+        return .init([
+            .init(icon: .init(named: "mountain")!,
+                  title: "Search until found",
+                  description: "Over a hundred insignia of the Swiss Armed Forces – each redesigned from the ground up."),
+            .init(icon: .init(named: "mountain")!,
+                  title: "Enlist prepared",
+                  description: "Practice with the app and pass the rank test on the first run."),
+            .init(icon: .init(named: "mountain")!,
+                  title: "#teamarmee",
+                  description: "Add name tags of your comrades or cadre. Insignia automatically keeps every name tag you create in iCloud.")
+        ])
+    }
+
+    // Notice Text
+    static func setUpNotice() -> UIOnboardingTextViewConfiguration {
+        return .init(icon: .init(named: "onboarding-notice-icon")!,
+                     text: "Developed and designed for members of the Swiss Armed Forces.",
+                     linkTitle: "Learn more...",
+                     link: "https://www.lukmanascic.ch/portfolio/insignia",
+                     tint: .init(named: "camou"))
+    }
+
+    // Continuation Title
+    static func setUpButton() -> UIOnboardingButtonConfiguration {
+        return .init(title: "Continue",
+                     titleColor: .white, // Optional, `.white` by default
+                     backgroundColor: UIColor.blue)
+    }
+}
