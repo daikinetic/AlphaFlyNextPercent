f29ade5e29eef6360cb7192064b8ed9a1789cf86
daikinetic <dddaiki.geek@gmail.com>
2023-11-01 23:09:26 +0900

    Enum Constructor

---
+++ AlphaFlyNextPercent/2023/EnumConstructor.swift
@@ -0,0 +1,59 @@
+//
+//  EnumConstructor.swift
+//  AlphaFlyNextPercent
+//
+//  Created by Daiki Takano on 2023/11/01.
+//
+//  https://qiita.com/_mpon/items/4491d19fa50b2039af35
+
+import Foundation
+
+enum Size {
+  case Small
+  case Medium
+  case Large
+}
+
+enum _Size {
+  case Small
+  case Medium
+  case Large
+  
+  init() {
+    self = .Small
+  }
+}
+
+
+enum Size_ {
+  case Small, Medium, Large
+
+  // 引数ありコンストラクタ
+  init(height: Int) {
+    if height < 160 {
+      self = .Small
+    } else if height < 170 {
+      self = .Medium
+    } else {
+      self = .Large
+    }
+  }
+}
+
+enum _Size_ {
+  case Small, Medium, Large
+
+  // 失敗するかもしれないコンストラクタ
+  init?(height: Int) {
+    if height < 160 {
+      self = .Small
+    } else if height < 170 {
+      self = .Medium
+    } else if height < 180 {
+      self = .Large
+    } else {
+      return nil
+    }
+  }
+}
+
