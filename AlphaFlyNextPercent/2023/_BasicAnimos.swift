9f77e1b0bc51d8bf6208d6da88a4c64dd61d2070
daikinetic <dddaiki.geek@gmail.com>
2023-11-10 21:55:24 +0900

    ComeFromLeft

--- AlphaFlyNextPercent/2023/_BasicAnimos.swift
+++ AlphaFlyNextPercent/2023/_BasicAnimos.swift
@@ -14,10 +14,12 @@ final class BasicAnimos: UIViewController {
   let containerView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 375.0, height: 667.0))
   let squareBlue = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 150.0, height: 150.0))
   let buttonPopIn = UIButton(type: .system)
+  let buttonDropFromTop = UIButton(type: .system)
+  let buttonComeFromLeft = UIButton(type: .system)
   let buttonResetPopIn = UIButton(type: .system)
   let buttonResetDropFromTop = UIButton(type: .system)
-  let buttonDropFromTop = UIButton(type: .system)
-  let offset = CGPoint(x: 0, y: -500)
+  let buttonResetComeFromLeft = UIButton(type: .system)
+  var offset = CGPoint(x: 0, y: -500)
   let x: CGFloat = 0, y: CGFloat = 0
 
   init() {
@@ -46,11 +48,18 @@ final class BasicAnimos: UIViewController {
     buttonDropFromTop.setTitle("DropFromTop", for: .normal)
     buttonDropFromTop.addTarget(self, action: #selector(DropFromTop(_:)), for: .touchDown)
 
+    buttonComeFromLeft.configuration = configurationBase
+    buttonComeFromLeft.setTitle("ComeFromLeft", for: .normal)
+    buttonComeFromLeft.addTarget(self, action: #selector(DropFromTop(_:)), for: .touchDown)
+
     buttonResetPopIn.configuration = configurationReset
     buttonResetPopIn.addTarget(self, action: #selector(Reset(_:)), for: .touchDown)
 
     buttonResetDropFromTop.configuration = configurationReset
     buttonResetDropFromTop.addTarget(self, action: #selector(ResetDFT(_:)), for: .touchDown)
+
+    buttonResetComeFromLeft.configuration = configurationReset
+    buttonResetComeFromLeft.addTarget(self, action: #selector(ResetCFL(_:)), for: .touchDown)
 
   }
   
@@ -73,6 +82,10 @@ final class BasicAnimos: UIViewController {
           HStackBlock(spacing: 10, alignment: .center) {
             buttonDropFromTop
             buttonResetDropFromTop
+          }
+          HStackBlock(spacing: 10, alignment: .center) {
+            buttonComeFromLeft
+            buttonResetComeFromLeft
           }
         }
 
@@ -148,13 +161,32 @@ extension BasicAnimos {
       initialSpringVelocity: 3,
       options: .curveEaseOut,
       animations: {
+        self.offset = CGPoint(x: 0, y: -500)
         self.squareBlue.alpha = 0
         self.squareBlue.transform = CGAffineTransform(translationX: self.offset.x + self.x, y: self.offset.y + self.y)
         self.squareBlue.isHidden = false
       },
       completion: nil
     )
-
   }
+
+  @objc func ResetCFL(_ sender: UIButton) {
+
+    UIView.animate(
+      withDuration: 1,
+      delay: 1,
+      usingSpringWithDamping: 0.47,
+      initialSpringVelocity: 3,
+      options: .curveEaseOut,
+      animations: {
+        self.offset = CGPoint(x: -500, y: 0)
+        self.squareBlue.alpha = 0
+        self.squareBlue.transform = CGAffineTransform(translationX: self.offset.x + self.x, y: self.offset.y + self.y)
+        self.squareBlue.isHidden = false
+      },
+      completion: nil
+    )
+  }
+
 }
 
