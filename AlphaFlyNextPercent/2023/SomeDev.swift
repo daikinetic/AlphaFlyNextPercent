c69e9e2f58b2d2874368e03aaf842b88f9761642
daikinetic <dddaiki.geek@gmail.com>
2023-11-11 23:46:14 +0900

    WIP

---
+++ AlphaFlyNextPercent/2023/SomeDev.swift
@@ -0,0 +1,9 @@
+//
+//  SomeDev.swift
+//  AlphaFlyNextPercent
+//
+//  Created by Daiki Takano on 2023/11/11.
+//
+
+import Foundation
+//
--- AlphaFlyNextPercent/2023/_BasicAnimos.swift
+++ AlphaFlyNextPercent/2023/_BasicAnimos.swift
@@ -13,14 +13,20 @@ final class BasicAnimos: UIViewController {
 
   let containerView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 375.0, height: 667.0))
   let squareBlue = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 150.0, height: 150.0))
+  //MARK: DisplayButton
   let buttonPopIn = UIButton(type: .system)
   let buttonDropFromTop = UIButton(type: .system)
   let buttonComeFromLeft = UIButton(type: .system)
   let buttonFadeIn = UIButton(type: .system)
+  let buttonCardFlip = UIButton(type: .system)
+
+  //MARK: ResetButton
   let buttonResetPopIn = UIButton(type: .system)
   let buttonResetDropFromTop = UIButton(type: .system)
   let buttonResetComeFromLeft = UIButton(type: .system)
   let buttonResetFadeIn = UIButton(type: .system)
+  let buttonResetCardFlip = UIButton(type: .system)
+
   var offset = CGPoint(x: 0, y: -500)
   let x: CGFloat = 0, y: CGFloat = 0
 
@@ -32,8 +38,6 @@ final class BasicAnimos: UIViewController {
     squareBlue.layer.cornerRadius = 20.0
     squareBlue.backgroundColor = UIColor.blue
     squareBlue.alpha = 0
-    squareBlue.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
-
 
     var configurationBase = UIButton.Configuration.gray()
     configurationBase.baseForegroundColor = .blue
@@ -42,6 +46,7 @@ final class BasicAnimos: UIViewController {
     configurationReset.baseForegroundColor = .red
     configurationReset.title = "Reset"
 
+    //MARK: DisplayButton
     buttonPopIn.configuration = configurationBase
     buttonPopIn.setTitle("PopIn", for: .normal)
     buttonPopIn.addTarget(self, action: #selector(PopIn(_:)), for: .touchDown)
@@ -58,6 +63,11 @@ final class BasicAnimos: UIViewController {
     buttonFadeIn.setTitle("FadeIn", for: .normal)
     buttonFadeIn.addTarget(self, action: #selector(FadeIn(_:)), for: .touchDown)
 
+    buttonCardFlip.configuration = configurationBase
+    buttonCardFlip.setTitle("CardFlip", for: .normal)
+    buttonCardFlip.addTarget(self, action: #selector(CardFlip(_:)), for: .touchDown) //
+
+    //MARK: ResetButton
     buttonResetPopIn.configuration = configurationReset
     buttonResetPopIn.addTarget(self, action: #selector(Reset(_:)), for: .touchDown)
 
@@ -69,6 +79,9 @@ final class BasicAnimos: UIViewController {
 
     buttonResetFadeIn.configuration = configurationReset
     buttonResetFadeIn.addTarget(self, action: #selector(ResetFI(_:)), for: .touchDown)
+
+    buttonResetCardFlip.configuration = configurationReset
+    buttonResetCardFlip.addTarget(self, action: #selector(ResetCF(_:)), for: .touchDown) //
 
   }
   
@@ -101,16 +114,17 @@ final class BasicAnimos: UIViewController {
             buttonResetFadeIn
           }
           HStackBlock(spacing: 10, alignment: .center) {
-            //Card Flip
+            buttonCardFlip
+            buttonResetCardFlip
           }
         }
 
       }
       .size(
         width: LayoutDescriptor.ConstraintValue(floatLiteral: view.bounds.width),
-        height: LayoutDescriptor.ConstraintValue(floatLiteral: view.bounds.width)
+        height: LayoutDescriptor.ConstraintValue(floatLiteral: 600)
       )
-      .padding(.vertical, 100)
+      .padding(.top, 100)
 
     }
   }
@@ -157,6 +171,20 @@ extension BasicAnimos {
       options: .curveEaseIn,
       animations: {
         self.squareBlue.transform = .identity
+        self.squareBlue.alpha = 1
+      },
+      completion: nil
+    )
+  }
+
+  @objc func CardFlip(_ sender: UIButton) {
+
+    UIView.animate(
+      withDuration: 0.5,
+      delay: 0.0,
+      options: .curveEaseInOut,
+      animations: {
+        self.squareBlue.transform = CGAffineTransform(scaleX: -1, y: 1)
         self.squareBlue.alpha = 1
       },
       completion: nil
@@ -229,6 +257,23 @@ extension BasicAnimos {
       options: .curveEaseOut,
       animations: {
         self.offset = CGPoint(x: 0, y: 0)
+        self.squareBlue.alpha = 0
+      },
+      completion: nil
+    )
+  }
+
+  @objc func ResetCF(_ sender: UIButton) {
+
+    UIView.animate(
+      withDuration: 0.5,
+      delay: 0.0,
+      usingSpringWithDamping: 0.47,
+      initialSpringVelocity: 3,
+      options: .curveEaseOut,
+      animations: {
+        self.offset = CGPoint(x: 0, y: 0)
+        self.squareBlue.transform = CGAffineTransform(scaleX: 1, y: 1)
         self.squareBlue.alpha = 0
       },
       completion: nil
