689486fe775656aca15dbdb937bff4ec31355d1c
daikinetic <dddaiki.geek@gmail.com>
2023-11-19 21:15:27 +0900

    Actor, Sendable

--- AlphaFlyNextPercent/2023/_Actor_Sendable.swift
+++ AlphaFlyNextPercent/2023/_Actor_Sendable.swift
@@ -84,7 +84,69 @@ extension BankAccount {
   }
 }
 
+// クロスアクター参照で class はサポートしていない
+final class Person {
+  var name: String
+  let birthDate: Date
+
+  init(name: String, birthDate: Date) {
+    self.name = name
+    self.birthDate = birthDate
+  }
+}
+
+actor _BankAccount {
+  var owners: [Person]
+
+  init(owners: [Person]) {
+    self.owners = owners
+  }
+
+  func primaryOwner() -> Person? { return owners.first }
+
+  // Actor 型の同一インスタンス内でのアクセスは可能
+  func primaryOwnerName() -> String? {
+    return primaryOwner()?.name
+  }
+}
+
+struct Actor_Sendable {
+  let account: _BankAccount
+
+  init(account: _BankAccount) {
+    self.account = account
+  }
+
+  func check() async {
+    // class 型（Person）を返すのでコンパイルエラー
+    if let primary = await account.primaryOwner() {
+      primary.name = "The Honorable " + primary.name
+    }
+  }
+}
 
 
+extension BankAccount {
 
-//WIP
+  func endOfMonth(month: Int, year: Int) {
+    // MARK: detach closure は @Sendable で定義されているため、
+    // MARK: closure 自身がアクター分離を行う.
+    detach {
+      // let transactions = await self.transactions(month: month, year: year)
+      // let report = Report(accountNumber: self.accountNumber, transactions: transaction)
+      // await report.email(to: self.accountOwnerEmailAddress)
+    }
+  }
+
+  func close(distributingTo accounts: [BankAccount]) async {
+    let transferAmount = balance / Double(accounts.count)
+
+    // MARK: forEach は @Sendable で定義されていないため、
+    // MARK: BankAccount (Actor型) がアクター分離を行う.
+
+    // accounts.forEach { account in
+      // balance = balance - transferAmount
+      // await account.deposit(amount: transferAmount)
+    // }
+  }
+}
