d69bdafc6f756cb34e757bac58a36f3843756a98
daikinetic <dddaiki.geek@gmail.com>
2024-01-05 22:16:37 +0900

    WIP

--- AlphaFlyNextPercent/2023/MapKitTrain.swift
+++ AlphaFlyNextPercent/2023/MapKitTrain.swift
@@ -7,6 +7,27 @@
 //  MapKit
 
 import Foundation
+import SwiftUI
 import MapKit
 
-//here
+fileprivate struct Home: View {
+  var body: some View {
+    Text("Hello, world")
+  }
+}
+
+#Preview {
+  Home()
+}
+
+extension CLLocationCoordinate2D {
+  static var myLocation: CLLocationCoordinate2D {
+    return .init(latitude: 37.3346, longitude: -122.0090)
+  }
+}
+
+extension MKCoordinateRegion {
+  static var myRegion: MKCoordinateRegion {
+    return .init(center: .myLocation, latitudinalMeters: 10000, longitudinalMeters: 10000)
+  }
+}
