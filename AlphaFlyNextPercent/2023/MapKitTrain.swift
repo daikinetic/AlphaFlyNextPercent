32510dddfb72736152a0fdc38281bb390f30c4d3
daikinetic <dddaiki.geek@gmail.com>
2024-01-10 23:19:16 +0900

    MapKit iOS17 WIP

--- AlphaFlyNextPercent/2023/MapKitTrain.swift
+++ AlphaFlyNextPercent/2023/MapKitTrain.swift
@@ -11,8 +11,65 @@ import SwiftUI
 import MapKit
 
 fileprivate struct Home: View {
+  @State private var cameraPosition: MapCameraPosition = .region(.myRegion)
+  @Namespace private var locationSpace
+  @State private var searchText: String = ""
+  @State private var showSearch: Bool = false
+  @State private var searchResults: [MKMapItem] = []
   var body: some View {
-    Text("Hello, world")
+    NavigationStack {
+      Map(position: $cameraPosition, scope: locationSpace) {
+        ///Map Annotations
+        Annotation("Apple Park", coordinate: .myLocation) {
+          ZStack {
+            Image(systemName: "applelogo")
+              .font(.title3)
+            Image(systemName: "square")
+              .font(.largeTitle)
+          }
+        }
+        .annotationTitles(.hidden)
+
+        ///Simply Display Annotations as Marker, as we seen before
+        ForEach(searchResults, id: \.self) { mapItem in
+          let placeMark = mapItem.placemark
+          Marker(placeMark.name ?? "Place", coordinate: placeMark.coordinate)
+        }
+
+        ///To Show User Current Location
+        UserAnnotation()
+      }
+      .overlay(alignment: .bottomTrailing) {
+        VStack(spacing: 15) {
+          MapCompass(scope: locationSpace)
+          MapPitchToggle(scope: locationSpace)
+          MapUserLocationButton(scope: locationSpace)
+        }
+        .buttonBorderShape(.circle)
+        .padding()
+      }
+      .mapScope(locationSpace)
+      .navigationTitle("Map")
+      .navigationBarTitleDisplayMode(.inline)
+      .searchable(text: $searchText, isPresented: $showSearch)
+      .toolbarBackground(.visible, for: .navigationBar)
+    }
+    .onSubmit(of: .search) {
+      Task {
+        guard !searchText.isEmpty else { return }
+
+        await searchPlaces()
+      }
+    }
+  }
+
+  func searchPlaces() async {
+    let request = MKLocalSearch.Request()
+    request.naturalLanguageQuery = searchText
+    request.region = .myRegion
+
+    let results = try? await MKLocalSearch(request: request).start()
+    searchResults = results?.mapItems ?? []
   }
 }
 
