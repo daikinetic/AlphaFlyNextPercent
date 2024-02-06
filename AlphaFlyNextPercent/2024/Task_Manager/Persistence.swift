702b2528ff5585f0c6d307708565103131cbd9e3
daikinetic <dddaiki.geek@gmail.com>
2024-02-06 15:21:04 +0900

    TaskManager+CoreData WIP

---
+++ AlphaFlyNextPercent/2024/Task_Manager/Persistence.swift
@@ -0,0 +1,26 @@
+//
+//  Persistence.swift
+//  AlphaFlyNextPercent
+//
+//  Created by Daiki Takano on 2024/02/06.
+//
+//  https://qiita.com/Saayaman/items/ea437032afaeddd0cf50
+//  立ち上げ直した方がいい
+
+import CoreData
+
+struct PersistenceController {
+  static let shared = PersistenceController()
+
+//  static var preview: PersistenceController {
+//
+//  }()
+
+  let container : NSPersistentContainer
+
+  init() {
+    container = NSPersistentContainer(name: "Task_Manager")
+
+
+  }
+}
---
+++ AlphaFlyNextPercent/2024/Task_Manager/TaskManager.swift
@@ -0,0 +1,19 @@
+//
+//  TaskManager.swift
+//  AlphaFlyNextPercent
+//
+//  Created by Daiki Takano on 2024/02/06.
+//
+//  https://youtu.be/x8h6hTqo2So?si=obVuXHq_u5QvJNOY
+
+import SwiftUI
+
+struct TaskManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskManager()
+}
